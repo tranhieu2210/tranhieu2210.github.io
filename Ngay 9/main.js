1f92a0d0a0f34d82d18b349b7e46eb71a296ca2f
tranhieu2210 <tth221097@gmail.com>
2021-11-17 12:04:04 +0700

    a

---
+++ Ngay 9/main.js
@@ -0,0 +1,69 @@
+console.log("Xin chào");
+
+
+let number 
+console.log(number);
+
+number=10
+console.log(number);
+
+// vừa khai báo biến vừa khởi tạo giá trị
+
+let email = "tth221097@gmail.com";
+
+// khai báo biến sử dụng const
+
+const pi = 3.14
+console.log(pi);
+
+// kiểm tra kiểu dữ liệu của 1 biến
+console.log(typeof number)
+console.log(typeof email)
+
+let age;;
+console.log(typeof age);
+
+
+age = 30;
+console.log(typeof age);
+
+age = "30"
+console.log(typeof age);
+
+// string
+// khai báo string rỗng
+let fristname="Trần"
+let lastname = "Hiếu"
+let fullname = fristname + " " + lastname
+console.log(fullname);
+
+// Template string
+// ``
+// cho phép truyền biến, biểu thức, giá trị ..
+
+let message = `"Hà Nôi" hôm nay trời đep lắm`
+console.log(message);
+
+let year = 1997;
+let text = `Xin chào ${fullname} sinh năm ${year} năm nay ${2021-year} tuổi`
+console.log(text)
+
+// number
+let num1 = 12
+let num2 = 3
+
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(num1 / num2);
+console.log(num1 % num2);
+console.log(num1 ** num2);
+
+let a = 1;
+let b = 22;
+let name = 'John';
+
+alert(b + a);
+console.log(alert(b + a))   
+
+console.log(alert(a + name))                     
