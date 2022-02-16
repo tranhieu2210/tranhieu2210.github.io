5a4799a15d156e8803eba75dc1e962f5d6511ec3
tranhieu2210 <tth221097@gmail.com>
2022-02-16 12:03:05 +0700

    a

---
+++ project-restaurant/public/popup/popup.js
@@ -0,0 +1,23 @@
+let preveiwContainer = document.querySelector('.products-preview');
+let previewBox = preveiwContainer.querySelectorAll('.preview');
+
+document.querySelectorAll('.popular .box-container .box').forEach(box =>{
+    box.onclick = () =>{
+        console.log(123);
+    preveiwContainer.style.display = 'flex';
+    let name = box.getAttribute('data-name');
+    previewBox.forEach(preview =>{
+      let target = preview.getAttribute('data-target');
+      if(name == target){
+        preview.classList.add('active');
+      }
+    });
+  };
+});
+
+previewBox.forEach(close =>{
+  close.querySelector('.fa-times').onclick = () =>{
+    close.classList.remove('active');
+    preveiwContainer.style.display = 'none';
+  };
+});
