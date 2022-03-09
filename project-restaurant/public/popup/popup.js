ef407dddc845195ea3e2c64e55784ccc582543b3
tranhieu2210 <tth221097@gmail.com>
2022-03-09 11:40:34 +0700

    a

--- project-restaurant/public/popup/popup.js
+++ project-restaurant/public/popup/popup.js
@@ -1,24 +1,33 @@
 let preveiwContainer = document.querySelector('.products-preview');
 let previewBox = preveiwContainer.querySelectorAll('.preview');
+let aw1 = document.querySelector('.cart-total .btn');
+let aw2 = document.querySelector('.order .btn')
 
 
-document.querySelectorAll('.popular .box-container .box').forEach(box =>{
-    box.onclick = () =>{
+document.querySelectorAll('.popular .box-container .box').forEach(box => {
+  box.onclick = () => {
     preveiwContainer.style.display = 'flex';
     let name = box.getAttribute('data-name');
-    previewBox.forEach(preview =>{
+    previewBox.forEach(preview => {
       let target = preview.getAttribute('data-target');
-      if(name == target){
+      if (name == target) {
         preview.classList.add('active');
       }
     });
   };
 });
 
-previewBox.forEach(close =>{
-  close.querySelector('.fa-times').onclick = () =>{
+previewBox.forEach(close => {
+  close.querySelector('.fa-times').onclick = () => {
     close.classList.remove('active');
     preveiwContainer.style.display = 'none';
   };
 });
 
+aw1.addEventListener('click', function () {
+  alert("Đặt hàng thành công")
+})
+
+aw2.addEventListener('click', function () {
+  alert("Đặt hàng thành công")
+})
